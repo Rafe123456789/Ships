109e0bbb9e525fb76c21c10348b958e700f9df8f
334773769 <334773769@4XNW282.tdsb.on.ca>
2023-01-13 13:44:48 -0500

    adds side boxes for layout and changes color

--- BattleShipRR/src/drake/rafe/ShipDriver.java
+++ BattleShipRR/src/drake/rafe/ShipDriver.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -17,7 +18,12 @@ public class ShipDriver extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
-		VBox root = new VBox();
+		HBox root = new HBox();
+		VBox root1 = new VBox();
+		VBox root2 = new VBox();
+		VBox root3 = new VBox();
+		root1.setPrefWidth(150);
+		root.getChildren().addAll(root1, root2, root3);
 		
 		// hello
 		GridPane topGrid = new GridPane();
@@ -25,14 +31,14 @@ public class ShipDriver extends Application {
 		for (int i = 0; i < 10; i++) {
 			for (int l = 0; l < 10; l++) {
 				Button b = new Button();
-			
+				b.setStyle("-fx-background-color: #00bfff");
 				b.setPrefSize(40, 40);
 				System.out.println();
 				topGrid.add(b, i, l);
 			}
 		}
 		
-		root.getChildren().add(topGrid);
+		root2.getChildren().add(topGrid);
 		
 		GridPane botGrid = new GridPane();
 		
@@ -45,10 +51,10 @@ public class ShipDriver extends Application {
 			}
 		}
 		Rectangle tx = new Rectangle(400, 100);
-		root.getChildren().add(tx);
-		root.getChildren().add(botGrid);
-		root.setLayoutX(150);
-		System.out.println(root.getLayoutX());
+		root2.getChildren().add(tx);
+		root2.getChildren().add(botGrid);
+		root2.setLayoutX(150);
+		
 		Scene scene = new Scene(root, 700, 900);
 		
 		stage.setScene(scene);
