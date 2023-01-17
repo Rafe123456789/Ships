355bb2fbff48c7a78ce2d7c9dc5ede4ca1cb6449
334650934 <334650934@C-2UA51014D6.tdsb.on.ca>
2023-01-17 12:55:09 -0500

    test

---
+++ BattleShipRR/src/drake/rafe/MoveShips.java
@@ -0,0 +1,74 @@
+package drake.rafe;
+/**
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class MoveShips {
+	VBox ship2 = new VBox();
+	Button a = new Button();
+	Button b = new Button();
+	ship2.getChildren().addAll(a,b);
+	
+	//Rotate 
+	ship2.setOnMouseClicked(event -> {
+	    ship2.setRotate(ship2.getRotate() + 90);
+	});
+	
+	
+	VBox ship3a = new VBox();
+	Button a1 = new Button();
+	Button b1 = new Button();
+	Button c1 = new Button();
+	ship3a.getChildren().addAll(a1,b1,c1);
+	
+	ship3a.setOnMouseClicked(event -> {
+	    ship3a.setRotate(ship3a.getRotate() + 90);
+	});
+	
+	VBox ship3b = new VBox();
+	Button a2 = new Button();
+	Button b2 = new Button();
+	Button c2 = new Button();
+	ship3b.getChildren().addAll(a2,b2,c2);
+	
+	ship3a.setOnMouseClicked(event -> {
+	    ship3b.setRotate(ship3b.getRotate() + 90);
+	
+	VBox ship3c = new VBox();
+	Button a3 = new Button();
+	Button b3 = new Button();
+	Button c3 = new Button();
+	ship3c.getChildren().addAll(a3,b3,c3);
+	ship3c.setOnMouseClicked(event -> {
+	    ship3c.setRotate(ship3c.getRotate() + 90);
+	
+	VBox ship4a = new VBox();
+	Button a4 = new Button();
+	Button b4 = new Button();
+	Button c4 = new Button();
+	Button d4 = new Button();
+	ship4a.getChildren().addAll(a4,b4,c4,d4);
+	ship4a.setOnMouseClicked(event -> {
+	    ship4a.setRotate(ship4a.getRotate() + 90);
+	
+	VBox ship4b = new VBox();
+	Button a5 = new Button();
+	Button b5 = new Button();
+	Button c5 = new Button();
+	Button d5 = new Button();
+	ship4b.getChildren().addAll(a5,b5,c5,d5);
+	ship4b.setOnMouseClicked(event -> {
+	    ship4b.setRotate(ship4b.getRotate() + 90);
+	
+	VBox ship5 = new VBox();
+	Button a6 = new Button();
+	Button b6 = new Button();
+	Button c6 = new Button();
+	Button d6 = new Button();
+	Button e6 = new Button();
+	ship5.getChildren().addAll(a6,b6,c6,d6,e6);
+	ship5.setOnMouseClicked(event -> {
+	    ship5.setRotate(ship5.getRotate() + 90);
+}
+}
+*/
